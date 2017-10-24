9a72adca8329ae5b88380857e2aca5b7ec1ad6d9
Ejlp Indonesia <ejlp12@gmail.com>
2017-10-24 07:04:02 +0700

    fix jdbc module directory structure

--- deploy-sample-app.sh
+++ deploy-sample-app.sh
@@ -4,7 +4,8 @@ oc login -u developer -p developer > /dev/null
 oc project myproject
 
 echo "---> Create new-app with datasource to ORACLE database"
-oc new-app s2i-jboss-as-711-centos7~https://github.com/ejlp12/HelloWebAppWAR.git \
+oc new-app s2i-jboss-as-711-centos7~https://github.com/ejlp12/queryrunner.git \
+--context-dir=war \
 --name=helloapp \
 -e DB_SERVICE_PREFIX_MAPPING=TEST_ORACLE \
 -e TEST_ORACLE_SERVICE_HOST=docker-oracle-xe \
@@ -22,3 +23,9 @@ sleep 5
 echo
 echo "---> Follow build progress..."
 oc logs -f bc/helloapp
+
+echo
+echo "---> Show logs..."
+oc logs $(oc get pods | grep helloapp- | grep -v build | grep -v deploy | grep Running | awk '{print $1}')
+
+echo
